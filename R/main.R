6c1d8877bdf1b5d60d28affb03bb9a5c4bc2ea63
Christian Werner <christian.wnr@gmail.com>
2019-06-17 11:21:07 +0200

    tpot section

--- R/main.R
+++ R/main.R
@@ -1,11 +1,21 @@
-autostatr <- function(data, target,
-                      type = c("regr","classif"),
-                      output_dir = "~/Desktop") {
+d <- iris
+d[c(1:10),5] <- NA
 
-  # Render the Report ---------------------------------------------------------
+autostatr <- function(data, data_to_predict=NULL, target,
+                      type = c("regr","classif"),
+                      output_dir = "~/Desktop",
+                      title) {
+  test <- data[is.na(data[target]),]
+  train <- data[!is.na(data[target]),]
+  if(!is.null(data_to_predict)){
+    test <- rbind(data_to_predict,test)
+  }
+  print(train)
+
+  #Render the Report ---------------------------------------------------------
   rmarkdown::render(system.file("rmarkdown", "Preprocessing.Rmd",
                                 package = "AutoStatR"),
-                    params = list(data = data, target = target, type = type),
+                    params = list(data = train, data_to_predict = test, target = target, type = type, title = title),
                     output_dir = output_dir)
 }
 
