a920b67318b6480f2d738970d9fc9d7b65e29b07
Christopher Patrick Olbrich <c_olbr02@uni-muenster.de>
2019-06-21 15:10:51 +0200

    Extended the examples.

--- inst/example/cancer.R
+++ inst/example/cancer.R
@@ -1,8 +1,27 @@
-#Preprocessing
-data <- read.csv(url("https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data"), header = FALSE)
-colnames(data) <- c('Sample code number', 'Clump Thickness','Uniformity of Cell Size','Uniformity of Cell Shape','Marginal Adhesion','Single Epithelial Cell Size','Bare Nuclei','Bland Chromatin','Normal Nucleoli','Mitoses','Class')
-missing <- (apply(data, 1, function(x){any(x == "?")}))
-data <- data[!missing,]
-data$"Bare Nuclei" <- as.integer(as.character(data$"Bare Nuclei"))
-data <- data[,c(2:11)]
+require("caTools")
 
+# Preprocessing ---------------------------------------------------------------
+df <- read.csv(url("https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data"), header = FALSE)
+colnames(df) <- c('SampleCodenumber', 'ClumpThickness', 'UniformityOfCellSize',
+                  'UniformityofCellShape', 'MarginalAdhesion', 'SingleEpithelialCellSize',
+                  'BareNuclei', 'BlandChromatin', 'NormalNucleoli', 'Mitoses', 'Class')
+missing <- (apply(df, 1, function(x){any(x == "?")}))
+df <- df[!missing,]
+df$"BareNuclei" <- as.integer(as.character(df$"BareNuclei"))
+df <- df[,c(2:11)]
+
+# Replace numerical values by string to avoid FeatureImp error ----------------
+df[df[]["Class"] == 2, "Class"] <- "two"
+df[df[]["Class"] == 4, "Class"] <- "four"
+
+# Training and Test Split -----------------------------------------------------
+set.seed(101)
+sample = sample.split(df$"ClumpThickness", SplitRatio = 2/3)
+train = subset(df, sample == TRUE)
+test  = subset(df, sample == FALSE)
+test[,10] <- NA
+data = rbind(train,test)
+str(data)
+
+# Call the automatic statistician ---------------------------------------------
+autostatr(data=data, target="Class", type="classif", title="Breast Cancer")
--- inst/example/iris.R
+++ inst/example/iris.R
@@ -1,10 +1,7 @@
-# The following code illustrates how the Automatic statitician can be used on
-# the iris data set.
-
-# load the iris data
+# Add NA columns to the iris set for prediction -------------------------------
 iris <- iris
-d <- iris
-d[c(1:10),5] <- NA
+data <- iris
+data[c(1:10),5] <- NA
 
-# Call the automatic statistician
-autostatr(data=d, target="Species", type="classif", title="Iris")
+# Call the automatic statistician ---------------------------------------------
+autostatr(data=data, target="Species", type="classif", title="Iris")
