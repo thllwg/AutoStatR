ed7396fd56fc59e6c4d106b77279210bbbc87708
Christopher Patrick Olbrich <c_olbr02@uni-muenster.de>
2019-07-03 18:39:19 +0200

    Improved ALE description

--- inst/example/diabetes.R
+++ inst/example/diabetes.R
@@ -6,10 +6,11 @@ df <- PimaIndiansDiabetes
 smp_size <- floor(0.99 * nrow(df))
 
 # Set the seed to make partition reproducible ---------------------------------
-set.seed(123)
+set.seed(43)
 train_ind <- sample(seq_len(nrow(df)), size = smp_size)
 train <- df[train_ind, ]
 test <- df[-train_ind, ]
 
 # Call the automatic statistician ---------------------------------------------
 autostatr(data = train, data_to_predict = test, target = "diabetes", type = "classif", title = "Diabetes")
+
